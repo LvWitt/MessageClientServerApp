7faa138e7665e299a8b50e9ec26ba13824a55694
LvWitt <leovitorwitt@gmail.com>
2023-12-11 20:54:18 -0300

    Adicionar arquivos de projeto.

---
+++ INTERFACE/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace INTERFACE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ClientConnectBtn = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.LogSection = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ChatSection = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.InputChat = new System.Windows.Forms.TextBox();
+            this.SendMsgChatBtn = new System.Windows.Forms.Button();
+            this.ExitBtn = new System.Windows.Forms.Button();
+            this.clientsList = new System.Windows.Forms.CheckedListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.inputName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // ClientConnectBtn
+            // 
+            this.ClientConnectBtn.Location = new System.Drawing.Point(291, 38);
+            this.ClientConnectBtn.Name = "ClientConnectBtn";
+            this.ClientConnectBtn.Size = new System.Drawing.Size(99, 23);
+            this.ClientConnectBtn.TabIndex = 1;
+            this.ClientConnectBtn.Text = "Conectar";
+            this.ClientConnectBtn.UseVisualStyleBackColor = true;
+            this.ClientConnectBtn.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(81, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(201, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "IP Servidor";
+            // 
+            // LogSection
+            // 
+            this.LogSection.Location = new System.Drawing.Point(408, 34);
+            this.LogSection.Multiline = true;
+            this.LogSection.Name = "LogSection";
+            this.LogSection.ReadOnly = true;
+            this.LogSection.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.LogSection.Size = new System.Drawing.Size(379, 146);
+            this.LogSection.TabIndex = 4;
+            this.LogSection.TextChanged += new System.EventHandler(this.LogSection_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(406, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "LOG";
+            // 
+            // ChatSection
+            // 
+            this.ChatSection.Location = new System.Drawing.Point(18, 94);
+            this.ChatSection.Multiline = true;
+            this.ChatSection.Name = "ChatSection";
+            this.ChatSection.ReadOnly = true;
+            this.ChatSection.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.ChatSection.Size = new System.Drawing.Size(379, 267);
+            this.ChatSection.TabIndex = 6;
+            this.ChatSection.TextChanged += new System.EventHandler(this.ChatSection_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Chat";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // InputChat
+            // 
+            this.InputChat.Location = new System.Drawing.Point(19, 376);
+            this.InputChat.Multiline = true;
+            this.InputChat.Name = "InputChat";
+            this.InputChat.Size = new System.Drawing.Size(316, 48);
+            this.InputChat.TabIndex = 8;
+            // 
+            // SendMsgChatBtn
+            // 
+            this.SendMsgChatBtn.Location = new System.Drawing.Point(341, 376);
+            this.SendMsgChatBtn.Name = "SendMsgChatBtn";
+            this.SendMsgChatBtn.Size = new System.Drawing.Size(57, 48);
+            this.SendMsgChatBtn.TabIndex = 9;
+            this.SendMsgChatBtn.Text = "Enviar";
+            this.SendMsgChatBtn.UseVisualStyleBackColor = true;
+            this.SendMsgChatBtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ExitBtn
+            // 
+            this.ExitBtn.Location = new System.Drawing.Point(291, 65);
+            this.ExitBtn.Name = "ExitBtn";
+            this.ExitBtn.Size = new System.Drawing.Size(99, 23);
+            this.ExitBtn.TabIndex = 10;
+            this.ExitBtn.Text = "Desconectar";
+            this.ExitBtn.UseVisualStyleBackColor = true;
+            this.ExitBtn.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // clientsList
+            // 
+            this.clientsList.FormattingEnabled = true;
+            this.clientsList.Location = new System.Drawing.Point(409, 205);
+            this.clientsList.Name = "clientsList";
+            this.clientsList.Size = new System.Drawing.Size(144, 154);
+            this.clientsList.TabIndex = 11;
+            this.clientsList.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(406, 189);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Members";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(16, 14);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Nome";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // inputName
+            // 
+            this.inputName.Location = new System.Drawing.Point(81, 11);
+            this.inputName.Name = "inputName";
+            this.inputName.Size = new System.Drawing.Size(201, 20);
+            this.inputName.TabIndex = 13;
+            this.inputName.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.inputName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.clientsList);
+            this.Controls.Add(this.ExitBtn);
+            this.Controls.Add(this.SendMsgChatBtn);
+            this.Controls.Add(this.InputChat);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.ChatSection);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.LogSection);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.ClientConnectBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ClientConnectBtn;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox LogSection;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox ChatSection;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox InputChat;
+        private System.Windows.Forms.Button SendMsgChatBtn;
+        private System.Windows.Forms.Button ExitBtn;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox inputName;
+        private System.Windows.Forms.CheckedListBox clientsList;
+    }
+}
+
---
+++ INTERFACE/Form1.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+using Newtonsoft.Json;
+
+namespace INTERFACE
+{
+    public partial class Form1 : Form
+    {
+        static TcpClient tcpClient;
+        private NetworkStream stream;
+        static Dictionary<string, string> Onlineclients = new Dictionary<string, string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            tcpClient = new TcpClient("127.0.0.1", 55555);
+            stream = tcpClient.GetStream();
+            LogSection.Text += $"Conectando ao servidor...{Environment.NewLine}";
+
+
+            SendData(inputName.Text);
+
+            // Inicia uma thread para receber mensagens
+            Thread receiveThread = new Thread(delegate () { ReceiveMessages(); });
+            receiveThread.Start();
+
+        }
+
+        void ReceiveMessages()
+        {
+            byte[] buffer = new byte[1024];
+
+            while (true)
+            {
+                try
+                {
+                    Array.Clear(buffer, 0, buffer.Length);
+                    int bytes = stream.Read(buffer, 0, buffer.Length);
+
+                    string receivedData = Encoding.UTF8.GetString(buffer, 0, bytes);
+
+                    if (receivedData == null)
+                        break;
+
+                    // Verifica o prefixo da mensagem recebida
+                    if (receivedData.StartsWith("DICT-"))
+                    {
+                        string[] partes = receivedData.Split(new string[] { "DICT-", ":", "/", "-MSG:" }, StringSplitOptions.None);
+
+                        // Armazena cada parte em sua respectiva variável
+                        string ip = partes[1];
+                        string porta = partes[2];
+                        string nome = partes[3];
+                        receivedData = partes[4];
+
+                    }
+
+                    if (receivedData.StartsWith("-MSG"))
+                    {
+                        string[] parte = receivedData.Split(new string[] {"-MSG:"}, StringSplitOptions.None);
+                        receivedData = parte[0];
+                    }
+
+                    Console.WriteLine(receivedData);
+
+                    this.Invoke((MethodInvoker)delegate ()
+                    {
+                        ChatSection.Text += $"{receivedData}{Environment.NewLine}";
+                    });
+
+
+                }
+                catch
+                {
+                    Console.WriteLine("Erro +55");
+                    break;
+                }
+            }
+        }
+
+        public void UpdateOwnDictionary(Dictionary<string, string> dicionarioRecebido)
+        {
+            foreach (var par in dicionarioRecebido)
+            {
+                // Se a chave já existe, atualiza o valor; caso contrário, adiciona a nova entrada
+                Onlineclients[par.Key] = par.Value;
+            }
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SendData(InputChat.Text);
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LogSection_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        void SendData(string message)
+        {
+
+            if (!string.IsNullOrEmpty(message))
+            {
+                if (clientsList.CheckedItems.Count > 0)
+                {
+                    foreach (var item in clientsList.CheckedItems)
+                    {
+                        // Aqui, estou assumindo que cada 'item' é uma string contendo o endereço IP e a porta no formato "IP:Porta"
+                        string[] parts = item.ToString().Split(':');
+                        if (parts.Length == 2)
+                        {
+                            string ip = parts[0];
+                            int port = int.Parse(parts[1]);
+
+                            // Chama a função para enviar a mensagem para o cliente especificado
+                            SendMessageToClient(ip, port, message);
+                        }
+                    }
+                }
+                else
+                {
+                    byte[] data = Encoding.UTF8.GetBytes(message);
+                    stream.Write(data, 0, data.Length);
+                }
+            }
+        }
+
+        public void SendMessageToClient(string ipAddress, int port, string message)
+        {
+            try
+            {
+                // Cria um TcpClient conectado ao endereço IP e porta fornecidos
+                using (TcpClient client = new TcpClient(ipAddress, port))
+                {
+                    // Obtém o NetworkStream associado ao TcpClient
+                    using (NetworkStream stream = client.GetStream())
+                    {
+                        // Converte a mensagem em um array de bytes
+                        byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+
+                        // Envia a mensagem para o cliente
+                        stream.Write(messageBytes, 0, messageBytes.Length);
+                        Console.WriteLine($"Mensagem enviada para {ipAddress}:{port}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Trata qualquer exceção que possa ocorrer durante a conexão ou envio
+                Console.WriteLine($"Erro ao enviar mensagem para {ipAddress}:{port} - {ex.Message}");
+            }
+        }
+
+        public void EnviarMensagemParaCliente(string endereco, string mensagem)
+        {
+            // Separa o IP e a porta com base no caractere ':'
+            string[] partes = endereco.Split(':');
+            string clientIP = partes[0];
+            int clientPort = int.Parse(partes[1]);
+
+            // Converte a mensagem em bytes
+            byte[] data = Encoding.UTF8.GetBytes(mensagem);
+
+            // Cria um TcpClient e conecta ao cliente
+            TcpClient client = new TcpClient(clientIP, clientPort);
+
+            // Obtém o NetworkStream
+            NetworkStream stream = client.GetStream();
+
+            // Envia os dados para o cliente
+            stream.Write(data, 0, data.Length);
+
+            // Fecha o stream e o cliente
+            stream.Close();
+            client.Close();
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            tcpClient.Close();
+        }
+
+        private void ChatSection_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
